5f75c27a3cd26861e75429225dbcd6f2049bf14e
Louka013 <louka.pain0205@gmail.com>
2025-07-03 16:33:10 +0200

    modifs et ajout d'un programme pour pouvoir executer des fichiers en c

--- itemtracker/package/item-tracker/item-tracker.mk
+++ itemtracker/package/item-tracker/item-tracker.mk
@@ -4,8 +4,12 @@ ITEM_TRACKER_VERSION = 1.1
 ITEM_TRACKER_SITE_METHOD = local
 ITEM_TRACKER_SITE = $(BR2_EXTERNAL_ITEMTRACKER_PATH)/package/item-tracker/src
 
+define ITEM_TRACKER_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
 define ITEM_TRACKER_INSTALL_TARGET_CMDS
-#	$(INSTALL) -m 0755 $(@D)/item-tracker $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -m 0755 $(@D)/item-tracker $(TARGET_DIR)/usr/sbin
 	$(INSTALL) -m 0755 $(ITEM_TRACKER_PKGDIR)/S80item-tracker $(TARGET_DIR)/etc/init.d
 endef
 
